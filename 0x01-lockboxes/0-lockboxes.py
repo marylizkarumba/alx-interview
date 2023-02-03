ec956987316973c6d40ca4080436139f09e453e6
marylizkarumba <marylizkarumba791@gmail.com>
2023-02-03 14:43:18 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 """
-You have n number of locked boxes in front of you.
-Each box is numbered sequentially from 0 to n - 1
-and each box may contain keys to the other boxes.
-boxes is a list of lists
-A key with the same number as a box opens that box
-You can assume all keys will be positive integers
-The first box boxes[0] is unlocked
-Return True if all boxes can be opened, else return False
+Module that determines if all the boxes can be opened.
 """
 
 
 def canUnlockAll(boxes):
-    """ method that determines if all the boxes can be opened. """
-    n = len(boxes)
-    myList = [0]
-    for i in myList:
-        for j in boxes[i]:
-            if j not in myList:
-                if j < n:
-                    myList.append(j)
-    if len(myList) == n:
-        return True
+    """
+    Prototype that determines if all the boxes can be opened.
+    """
+    size = len(boxes)  # size of the list of boxes.
+    checkbox = {}  # dictionary that will contain the boxes that can be opened.
+    index = 0  # index of the box that will be checked.
+
+    for keys in boxes:  # for each box in the list of boxes.
+        if len(keys) == 0 or index == 0:
+            checkbox[index] = -1
+        for key in keys:
+            if key < size and key != index:
+                checkbox[key] = key  # the box is added to the dictionary.
+        if len(checkbox) == size:
+            return True  # all the boxes can be opened.
+        index += 1
     return False
